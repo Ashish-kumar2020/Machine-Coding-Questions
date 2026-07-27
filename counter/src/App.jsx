6f69d5c663997c0ed26ff42d0eb9a59566d62db5
Ashish-kumar2020 <ashishsinghk2020@gmail.com>
2026-07-27 23:49:15 +0530

    Learn about redux using counter app

---
+++ counter/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DES" });
+  };
+  const addBy = () => {
+    dispatch({type: "ADD",payload:10})
+  }
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+       <button onClick={addBy}>Add By 10</button>
+    </>
+  );
+}
+
+export default App;

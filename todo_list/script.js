d013b37e31b43f960fbe7e977f6c43c4ba472efd
Ashish-kumar2020 <ashishsinghk2020@gmail.com>
2026-08-25 01:00:35 +0530

    refactore code

--- todo_list/script.js
+++ todo_list/script.js
@@ -6,118 +6,217 @@ class TodoList {
     this.nextId = 1;
   }
 
+
   init() {
     const userInput = document.querySelector("#userInput");
     const addTodoBtn = document.querySelector("#addTodoBtn");
+    const todosUL = document.querySelector(".todos");
+
+    // Add Todo
     addTodoBtn.addEventListener("click", () => {
       const value = userInput.value.trim();
-      if (value.length === 0) {
-        alert("Todo Can not be empty");
+
+      if (!value) {
+        alert("Todo cannot be empty");
         return;
       }
+
       this.addTodo(value);
+
+      userInput.value = "";
+
       this.render();
+    });
+
+
+    todosUL.addEventListener("click", (e) => {
+      const action = e.target.dataset.action;
+
+      if (!action) return;
+
+      const todoId = Number(e.target.dataset.id);
+
+     
+      if (action === "delete") {
+        this.deleteTodo(todoId);
+      }
+
+  
+      if (action === "edit") {
+        const li = e.target.closest("li");
+
+        const editButton = li.querySelector('[data-action="edit"]');
+        const saveButton = li.querySelector('[data-action="save"]');
+        const spanText = li.querySelector(".todo-text");
+        const editInput = li.querySelector(".edit-input");
+
+        spanText.style.display = "none";
+        editButton.style.display = "none";
+
+        editInput.style.display = "inline";
+        saveButton.style.display = "inline";
+
+        editInput.focus();
+      }
+
+      if (action === "save") {
+        const li = e.target.closest("li");
+
+        const editInput = li.querySelector(".edit-input");
+
+        this.editTodo(todoId, editInput.value);
+      }
+    });
+
+  
+    todosUL.addEventListener("change", (e) => {
+      const action = e.target.dataset.action;
+
+      if (action !== "toggle") return;
+
+      const todoId = Number(e.target.dataset.id);
+
+      this.toggleTodo(todoId, e.target.checked);
     });
   }
 
   addTodo(todo) {
     this.todos.push({
       id: this.nextId++,
+      todo,
       isCompleted: false,
-      todo: todo,
     });
   }
 
+
+  deleteTodo(todoId) {
+    this.todos = this.todos.filter(
+      (todo) => todo.id !== todoId
+    );
+
+    this.render();
+  }
+
+
+  editTodo(todoId, newValue) {
+    const todo = this.todos.find(
+      (todo) => todo.id === todoId
+    );
+
+    if (!todo) return;
+
+    const value = newValue.trim();
+
+    if (!value) {
+      alert("Todo cannot be empty");
+      return;
+    }
+
+    todo.todo = value;
+
+    this.render();
+  }
+
+  toggleTodo(todoId, isCompleted) {
+    const todo = this.todos.find(
+      (todo) => todo.id === todoId
+    );
+
+    if (!todo) return;
+
+    todo.isCompleted = isCompleted;
+  }
+
   createTagElement(elementName) {
-    const elementTag = document.createElement(elementName);
-    return elementTag;
+    return document.createElement(elementName);
   }
 
   render() {
     const todosUL = document.querySelector(".todos");
+
     todosUL.innerHTML = "";
+
     this.todos.forEach((todo) => {
-      let inputValue = this.createTagElement("li");
-      let spanText = this.createTagElement("span");
-      let completedEle = this.createTagElement("input");
-      let deleteButton = this.createTagElement("button");
-      let editButton = this.createTagElement("button");
-      completedEle.type = "checkbox";
+      const li = this.createTagElement("li");
+
+
+      const spanText = this.createTagElement("span");
+      spanText.className = "todo-text";
       spanText.textContent = todo.todo;
-      spanText.id = todo.id;
-      inputValue.id = todo.id;
+
+
+      const completedEle =
+        this.createTagElement("input");
+
+      completedEle.type = "checkbox";
+      completedEle.checked = todo.isCompleted;
+
       completedEle.dataset.id = todo.id;
+      completedEle.dataset.action = "toggle";
 
-      // Delete Button
-      deleteButton.textContent = "Delete Todo";
-      deleteButton.dataset.id = todo.id;
+      const editInput =
+        this.createTagElement("input");
 
-      // Edit Button
-      editButton.textContent = "Edit Todo";
-      editButton.dataset.id = todo.id;
-      editButton.style.marginLeft = "10px";
-      let editInput = this.createTagElement("input");
+      editInput.type = "text";
+      editInput.className = "edit-input";
+      editInput.value = todo.todo;
       editInput.style.display = "none";
 
-      const saveButton = this.createTagElement("button");
+      editInput.dataset.id = todo.id;
+
+
+      const editButton =
+        this.createTagElement("button");
+
+      editButton.textContent = "Edit Todo";
+
+      editButton.dataset.id = todo.id;
+      editButton.dataset.action = "edit";
+
+      const saveButton =
+        this.createTagElement("button");
+
       saveButton.textContent = "Save Todo";
+
+      saveButton.dataset.id = todo.id;
+      saveButton.dataset.action = "save";
+
       saveButton.style.display = "none";
-      saveButton.id = todo.id;
-      editButton.addEventListener("click", (e) => {
-        const todoId = Number(e.target.dataset.id);
-        let selectedTodo = this.todos.find((val) => val.id == todoId);
-        console.log(selectedTodo);
-        spanText.style.display = "none";
-        editButton.style.display = "none";
-        saveButton.style.display = "inline";
-        editInput.type = "text";
-        editInput.style.display = "inline";
-        editInput.value = selectedTodo.todo;
-        inputValue.append(saveButton);
-        inputValue.append(editInput);
-      });
 
-      saveButton.addEventListener("click", (e) => {
-        const todoId = Number(e.target.id);
-        let selectedTodo = this.todos.find((val) => val.id == todoId);
-        if (editInput.value.length === 0) {
-          alert("Todo cannot be empty");
-          return;
-        }
-        spanText.textContent = editInput.value;
-        selectedTodo.todo = editInput.value;
-        spanText.style.display = "inline";
-        editInput.style.display = "none";
-        editButton.style.display = "inline";
-        saveButton.style.display = "none";
-        this.render();
-      });
 
-      deleteButton.addEventListener("click", (e) => {
-        console.log(e.target.dataset.id);
-        let todoId = Number(e.target.dataset.id);
-        this.todos = this.todos.filter((todo) => todo.id !== todoId);
-        this.render();
-      });
-      completedEle.addEventListener("change", (e) => {
-        const todoId = e.target.dataset.id;
-        let selectedTodo = this.todos.find((val) => val.id == Number(todoId));
-        selectedTodo.isCompleted = e.target.checked;
-        console.log(this.todos);
-      });
-      inputValue.append(spanText);
-      inputValue.append(completedEle);
-      inputValue.append(deleteButton);
-      inputValue.append(editButton);
-      todosUL.append(inputValue);
+      const deleteButton =
+        this.createTagElement("button");
+
+      deleteButton.textContent = "Delete Todo";
+
+      deleteButton.dataset.id = todo.id;
+      deleteButton.dataset.action = "delete";
+
+  
+      li.append(
+        completedEle,
+        spanText,
+        editInput,
+        editButton,
+        saveButton,
+        deleteButton
+      );
+
+
+      todosUL.append(li);
     });
   }
 }
 
+
+
 const todoList = new TodoList();
+
 todoList.init();
+
 todoList.addTodo("Learn JavaScript");
 todoList.addTodo("Learn React");
 todoList.addTodo("Learn Frontend System Design");
-console.log(todoList.todos);
+
+
 todoList.render();

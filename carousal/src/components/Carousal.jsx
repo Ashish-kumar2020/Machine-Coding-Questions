216df9f1cbe69a62bc7aff7771a68cb883630337
Ashish-kumar2020 <ashishsinghk2020@gmail.com>
2026-08-28 21:45:04 +0530

    COmpleted Carousal

--- carousal/src/components/Carousal.jsx
+++ carousal/src/components/Carousal.jsx
@@ -1,51 +1,87 @@
 import { useEffect, useState } from "react";
 import { CAROUSAL_IMAGES } from "../data/assets";
-import {GalleryHorizontalEnd} from "lucide-react"
+import { GalleryHorizontalEnd } from "lucide-react";
 const Carousal = () => {
   const [currImageIndex, setCurrImagesIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
 
   const handleNextImage = () => {
-      
-    if (currImageIndex < CAROUSAL_IMAGES.length - 1) {
-      setCurrImagesIndex((prev) => prev +1);
-    }
+    setCurrImagesIndex((prev) => {
+      if (prev === CAROUSAL_IMAGES.length - 1) {
+        return 0;
+      } else {
+        return prev + 1;
+      }
+    });
   };
 
   const handlePrevImage = () => {
-  
-    if (currImageIndex > 0) {
-      setCurrImagesIndex((prev) => prev - 1);
-    }
+    setCurrImagesIndex((prev) => {
+      if (prev === 0) {
+        return CAROUSAL_IMAGES.length - 1;
+      } else {
+        return prev - 1;
+      }
+    });
   };
 
   const handleDotImageChange = (index) => {
+    setCurrImagesIndex(index);
+  };
 
-    setCurrImagesIndex(index)
+  const handleMouseEnter = () => {
+    setIsPaused(true);
+  };
 
-  }
+  const handleMouseLeave = () => {
+    setIsPaused(false);
+  };
 
-//   useEffect(() => {
-//     let timer = setInterval(() => {
-//       setCurrImagesIndex((prev) => {
-//         if (prev === CAROUSAL_IMAGES.length - 1) {
-//           return 0;
-//         }else{
-//             return prev + 1;
-//         }
-//       });
-//     }, 3000);
+  const handleKeyDownEvents = (e) => {
+ 
+    if (e.key === "ArrowLeft") {
+        handlePrevImage()
+    }
+    if(e.key === "ArrowRight"){
+        handleNextImage()
+    }
+  };
 
-//     return () => {
-//       clearInterval(timer);
-//     };
-//   },[]);
+  useEffect(() => {
+    if (isPaused) return;
+    const timer = setInterval(() => {
+      setCurrImagesIndex((prev) => {
+        if (prev === CAROUSAL_IMAGES.length - 1) {
+          return 0;
+        } else {
+          return prev + 1;
+        }
+      });
+    }, 3000);
 
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isPaused]);
+
+  useEffect(() => {
+    console.log("keyboard effect mounted");
+    window.addEventListener("keydown", handleKeyDownEvents);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDownEvents);
+    };
+  }, []);
 
   return (
-    <div className="carousal-main-container">
+    <div
+      className="carousal-main-container"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+       
+    >
       {/* Left Button Container */}
       <div className="left-btn-container">
-        <button type="button" onClick={handlePrevImage}>
+        <button type="button" onClick={handlePrevImage} aria-label="Previous-Image">
           Left
         </button>
       </div>
@@ -57,19 +93,29 @@ const Carousal = () => {
           alt={`Carousal-Image-${currImageIndex}`}
         />
         <ul className="dot-container">
-            {
-                CAROUSAL_IMAGES.map((_,index) => {
-                   return <li key={index}><button onClick={() => handleDotImageChange(index)} className={currImageIndex === index ? "activeDot" : "inActiveDot"}><GalleryHorizontalEnd/></button> </li>
-                })
-            }
-            
-           </ul>
+          {CAROUSAL_IMAGES.map((_, index) => {
+            return (
+              <li key={index}>
+                <button
+                  onClick={() => handleDotImageChange(index)}
+                  className={
+                    currImageIndex === index ? "activeDot" : "inActiveDot"
+                  }
+                  aria-current={currImageIndex === index ? "true" : undefined}
+                  aria-label={`Go to image ${index + 1}`}
+                >
+                  <GalleryHorizontalEnd />
+                </button>{" "}
+              </li>
+            );
+          })}
+        </ul>
       </div>
-       {/* <GalleryHorizontalEnd/> */}
+      {/* <GalleryHorizontalEnd/> */}
 
       {/* Right Button Container */}
       <div className="right-btn-container">
-        <button type="button" onClick={handleNextImage}>
+        <button type="button" onClick={handleNextImage} aria-label="Next-Image">
           Right
         </button>
       </div>

30343149d7359f1493f2fa4b7b51923dd1855b70
Ashish-kumar2020 <ashishsinghk2020@gmail.com>
2026-09-01 23:41:16 +0530

    Fix bugs

--- password_strength_indicator/src/component/PasswordStrength.tsx
+++ password_strength_indicator/src/component/PasswordStrength.tsx
@@ -1,45 +1,58 @@
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 
-type PasswordStrengthLevel = "Empty" | "Weak" | "Medium" | "Strong"
-type PasswordStrengthColorLevel = "red" | "orange" | "green" | "";
+type PasswordStrengthLevel =
+  | "Empty"
+  | "Weak"
+  | "Medium"
+  | "Strong"
+  | "Very Strong";
+type PasswordStrengthColorLevel = "red" | "orange" | "green" | "" | "cyan";
 
 const PasswordStrength = () => {
   const [userPassword, setUserPassword] = useState<string>("");
   const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
- 
 
   const handlePasswordVisibility = () => {
     setIsPasswordVisible((prev) => !prev);
   };
 
-  const passwordStrength = (userPassword: string) : PasswordStrengthLevel => {
+  const passwordStrength = (userPassword: string): PasswordStrengthLevel => {
     if (userPassword.length === 0) {
-     return "Empty";
+      return "Empty";
     }
-    else if (userPassword.length <= 3) {
+    let score = 0;
+    if (userPassword.length >= 8) score++;
+    if (/[A-Z]/.test(userPassword)) score++;
+    if (/\d/.test(userPassword)) score++;
+    if (/[!@#$%^&*(),.?":{}|<>"]/.test(userPassword)) score++;
+
+    if (score === 1) {
       return "Weak";
-    }
-    else if ( userPassword.length <= 6) {
+    } else if (score === 2) {
       return "Medium";
+    } else if (score === 3) {
+      return "Strong";
     }
-    else {
-    return "Strong"
-    }
+    return "Very Strong";
   };
 
-  const getPasswordMeterColor = (strength: PasswordStrengthLevel):PasswordStrengthColorLevel  => {
-    switch(strength){
-        case "Empty":
-            return "";
-        case "Weak":
-            return "red";
-        case "Medium":
-            return "orange";
-        case "Strong":
-            return "green";
+  const getPasswordMeterColor = (
+    strength: PasswordStrengthLevel,
+  ): PasswordStrengthColorLevel => {
+    switch (strength) {
+      case "Empty":
+        return "";
+      case "Weak":
+        return "red";
+      case "Medium":
+        return "orange";
+      case "Strong":
+        return "green";
+      case "Very Strong":
+        return "cyan";
     }
-  }
+  };
 
   const strength = passwordStrength(userPassword);
   const strengthColor = getPasswordMeterColor(strength);
@@ -53,7 +66,7 @@ const PasswordStrength = () => {
           placeholder="Enter your password"
           id="userpassword"
           value={userPassword}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setUserPassword(e.target.value)
           }
         />
@@ -70,9 +83,10 @@ const PasswordStrength = () => {
         </button>
       </div>
       <div className="strength-container">
-        <div className="strength-meter" style={{backgroundColor: strengthColor}}>
-         
-        </div>
+        <div
+          className="strength-meter"
+          style={{ backgroundColor: strengthColor }}
+        ></div>
       </div>
       <div className="strength-text">
         <span>Strength : {strength}</span>
